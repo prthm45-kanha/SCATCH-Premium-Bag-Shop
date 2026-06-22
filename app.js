87be86e1105edac18691cc0193f201874c994a50
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-22 16:06:41 +0530

    setup basic boiler plate

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path=require('path');
 const jwt=require('jsonwebtoken');
 const bcrypt=require('bcrypt');
 const cookieParser=require('cookie-parser');
+const userModel=require('./models/user-model');
 app.use(cookieParser());
 
 app.set('view engine','ejs');
@@ -13,7 +14,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
-    res.send('Ram Ram Ji');
+    res.render('index');
 })
 app.listen(3000,()=>{
     console.log('server runnung on port 3000');

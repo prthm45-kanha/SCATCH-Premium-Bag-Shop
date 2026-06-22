49e3f68eea5c8c37e45c65419f219d495306d25d
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-22 16:56:05 +0530

    "22nd June-added separation of concerns in models and created owner model

--- app.js
+++ app.js
@@ -8,14 +8,21 @@ const cookieParser=require('cookie-parser');
 const userModel=require('./models/user-model');
 app.use(cookieParser());
 
+const ownersRouter=require('./routes/ownersRouter');
+const productsRouter=require('./routes/productsRouter');
+const usersRouter=require('./routes/usersRouter');
+
+const db= require('./config/mongoose-connection');
+
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
+
 app.listen(3000,()=>{
     console.log('server runnung on port 3000');
 })
---
+++ routes/usersRouter.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/',(req,res)=>{
+    res.send("hey it's workinggg")
+})
+module.exports=router;
